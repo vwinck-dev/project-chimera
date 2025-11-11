16c1be535f694128ffd85f8dd2b034d6301462ee
vwinck-dev <vwincknascimento@gmail.com>
2025-11-10 22:39:43 -0300

    feat: set up prettierrc, eslint and husky for development

---
+++ config/app.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+
+export const config = {
+  token: process.env.DISCORD_TOKEN,
+  clientId: process.env.CLIENT_ID,
+  guildId: process.env.GUILD_ID,
+  lang: process.env.APP_LANGUAGE || 'en',
+  debugMode: process.env.DEBUG_MODE === 'true',
+  db: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  },
+  prefix: process.env.PREFIX || '/',
+};
